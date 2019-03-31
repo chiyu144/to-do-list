aeaeb6c1fb7fa5a2ba36c26921e2c01c6eed9d06
Julie <sizumasa@hotmail.com>
2019-03-31 14:18:54 +0800

    add allDone & clearCompleted feature

--- src/index.js
+++ src/index.js
@@ -7,26 +7,42 @@ import shortid from 'shortid';
 
 class App extends Component {
     constructor() {
-        const prevTodoList = JSON.parse(localStorage.getItem("todoList"));
         super();
+
+        const prevTodoList = JSON.parse(localStorage.getItem("todoList"));
+        // const remainingNum = prevTodoList.length;
+        // let remaining = function() {
+        //     if (remainingNum === 1) return 1 + ' Task Left'
+        //     else return remainingNum + ' Tasks Left'
+        // }
+
         this.removeTaskHandler = this.removeTaskHandler.bind(this);
         this.checkClickHandler = this.checkClickHandler.bind(this);
         this.state = {
             placeholder: "Add Task To List…_〆(°▽°*)",
-            todoList: prevTodoList || []
+            todoList: prevTodoList || [],
+            // remaining: remaining() || 0 + ' Tasks Left'
         }
     }
 
     blur() { this.setState({ placeholder: "Add Task To List…_〆(°▽°*)" }) }
     focus() { this.setState({ placeholder: "" }) }
+    
+    toggleNav(e) { 
+        let nav = e.currentTarget.childNodes[0];
+        while(nav) {
+            if(nav.tagName === 'A') { nav.classList.remove('navActive') }
+            nav = nav.nextSibling;
+        }
+        e.target.classList.add('navActive');
+    }
 
     removeTaskHandler(e) {
         this.setState({ 
             todoList: this.state.todoList.filter((task) => {
                 return task.id !== e.target.parentNode.id;
             })
-        },
-        () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) });
+        }, () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) });
     }
 
     checkClickHandler(e) {
@@ -37,8 +53,7 @@ class App extends Component {
                     return Object.assign(task, { checked: !task.checked })
                 } else { return task }
             })
-        }), () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) }
-        )
+        }), () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) })
     }
 
     addTaskHandler(e) {
@@ -53,12 +68,27 @@ class App extends Component {
                     description: taskElement.value,
                     checked: false
                 })
-            },
-            () => {
+            }, () => {
                 localStorage.setItem("todoList", JSON.stringify(this.state.todoList));
                 taskElement.value = "";
             });
         }
+    }
+
+    allDoneHandler() {
+        this.setState((prevState) => ({
+            todoList: prevState.todoList.map( task => {
+                return Object.assign(task, { checked: true })
+            })
+        }), () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) })
+    }
+
+    clearCompletedHandler() {
+        this.setState({ 
+            todoList: this.state.todoList.filter((task) => {
+                return task.checked !== true;
+            })
+        }, () => { localStorage.setItem("todoList", JSON.stringify(this.state.todoList)) });
     }
 
     render() {
@@ -74,20 +104,20 @@ class App extends Component {
                         onBlur={() => this.blur()} onFocus={() => this.focus()}/>
                     <button type="submit" id="addNoteBtn">Add</button>
                 </form>
-                <BrowserRouter basename={'/todolist'}>
+                <BrowserRouter basename={'/todolist'}> {/* basename={'/todolist'} */}
                     <nav>
-                        {/* <div className="guide">
-                            <p id="remaining"></p>
-                        </div> */}
                         <div className="guide">
-                            <Link to="/">All</Link>
-                            <Link to="/active">Active</Link>
-                            <Link to="/completed">Completed</Link>
+                            <p id="remaining"></p>
                         </div>
-                        {/* <div className="guide">
-                            <button id="allDone"></button>
-                            <button id="clearCompleted"></button>
-                        </div> */}
+                        <div className="guide" onClick={(e)=>this.toggleNav(e)}>
+                            <Link className="nav navActive" to="/">All</Link>
+                            <Link className="nav" to="/active">Active</Link>
+                            <Link className="nav" to="/completed">Completed</Link>
+                        </div>
+                        <div className="guide">
+                            <button onClick={(e)=>this.allDoneHandler(e)} id="allDone"></button>
+                            <button onClick={(e)=>this.clearCompletedHandler(e)} id="clearCompleted"></button>
+                        </div>
                     </nav>
                     <Route exact path="/" render={()=><All todoList={this.state.todoList} checked={everyChecked} checkClickHandler={this.checkClickHandler} removeTaskHandler={this.removeTaskHandler}/>}/>
                     <Route exact path="/active" render={()=><All todoList={activeTask} checked={everyChecked} checkClickHandler={this.checkClickHandler} removeTaskHandler={this.removeTaskHandler} />}/>
